179ce64f7d7071fb103b3e52bd7c48988f48983f
Isabella <isabella.y.hu@gmail.com>
2022-08-01 20:07:53 +1000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,7 @@
-# def square_array(array)
-#   # your code here
-#   result = []
-#   array.each {|number| result << number ** 2}
-#   result
-# end
-
 def square_array(array)
   # your code here
-  # result = []
-  array.collect {|number| number ** 2}
+  result = []
+  array.each {|number| result << number ** 2}
+  result
+  # array.collect {|number| number ** 2}
 end
